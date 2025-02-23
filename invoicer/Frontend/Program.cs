ebcf12471b3be77860f64e5f7a89394a1af4b8b7
S1lence <j3.zelenka@gmail.com>
2025-02-23 15:37:55 +0100

    feat: add nav menu items provider

--- invoicer/Frontend/Program.cs
+++ invoicer/Frontend/Program.cs
@@ -1,5 +1,6 @@
 using System.Net.Http.Json;
 using Frontend.Models;
+using Frontend.Utils;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -16,13 +17,16 @@ namespace Frontend
 			// Create a new http client
 			var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
 
-			// Load environment config
-			var envConfig = await httpClient.GetFromJsonAsync<EnvironmentConfig>("env.json");
+			// Load config files
+			var envConfig = await httpClient.GetFromJsonAsync<EnvironmentConfig>("./Data/env.json");
+			var navMenuItemsList = await httpClient.GetFromJsonAsync<List<NavLinkItem>>("./Data/NavMenuContent.json");
 
 			// Register services
 			builder.Services.AddScoped(sp => httpClient);
 			if (envConfig is not null)
 				builder.Services.AddSingleton(envConfig);
+			if (navMenuItemsList is not null)
+				builder.Services.AddSingleton(new NavMenuItemsProvider(navMenuItemsList));
 
 			// Build and run the app
 			await builder.Build().RunAsync();
