fc571e26140c823c35c82a0d501162cb6c48a936
S1lence-z <j3.zelenka@gmail.com>
2025-02-24 01:44:19 +0100

    chore: add sample address service for fe

---
+++ invoicer/Frontend/Api/AddressService.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Models;
+using Domain.ServiceInterfaces;
+using Frontend.Models;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+
+namespace Frontend.Api
+{
+	public class AddressService : IAddressService
+	{
+		private readonly HttpClient _httpClient;
+
+		public AddressService(EnvironmentConfig config)
+		{
+			_httpClient = new HttpClient { BaseAddress = new Uri(config.ApiBaseUrl) };
+		}
+
+		public async Task<Address> CreateAsync(Address obj)
+		{
+			Console.WriteLine(_httpClient.BaseAddress + "api/Address");
+			var response = await _httpClient.PostAsJsonAsync("api/Address", obj);
+			return await response.Content.ReadFromJsonAsync<Address>();
+		}
+
+		public async Task<bool> DeleteAsync(int id)
+		{
+			var response = await _httpClient.DeleteAsync($"api/Address/{id}");
+			return response.IsSuccessStatusCode;
+		}
+
+		public async Task<IList<Address>> GetAllAsync()
+		{
+			return await _httpClient.GetFromJsonAsync<IList<Address>>("api/Address");
+		}
+
+		public async Task<Address?> GetByIdAsync(int id)
+		{
+			return await _httpClient.GetFromJsonAsync<Address>($"api/Address/{id}");
+		}
+
+		public async Task<Address> UpdateAsync(int id, Address obj)
+		{
+			var response = await _httpClient.PutAsJsonAsync($"api/Address/{id}", obj);
+			return await response.Content.ReadFromJsonAsync<Address>();
+		}
+	}
+}
--- invoicer/Frontend/Program.cs
+++ invoicer/Frontend/Program.cs
@@ -1,8 +1,10 @@
 using System.Net.Http.Json;
 using Frontend.Models;
 using Frontend.Utils;
+using Frontend.Api;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Domain.ServiceInterfaces;
 
 namespace Frontend
 {
@@ -21,12 +23,16 @@ namespace Frontend
 			var envConfig = await httpClient.GetFromJsonAsync<EnvironmentConfig>("./Data/env.json");
 			var navMenuItemsList = await httpClient.GetFromJsonAsync<List<NavLinkItem>>("./Data/NavMenuContent.json");
 
-			// Register services
+			// Register config files
 			builder.Services.AddScoped(sp => httpClient);
 			if (envConfig is not null)
 				builder.Services.AddSingleton(envConfig);
 			if (navMenuItemsList is not null)
 				builder.Services.AddSingleton(new NavMenuItemsProvider(navMenuItemsList));
+
+			// Register api services
+			
+			builder.Services.AddScoped<IAddressService, AddressService>();
 
 			// Build and run the app
 			await builder.Build().RunAsync();
