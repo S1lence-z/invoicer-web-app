2bb9adad33e48aa637eb5ced2cd7d229d906f2a3
S1lence-z <j3.zelenka@gmail.com>
2025-02-24 01:03:16 +0100

    chore: add custom dockerfile for building

--- invoicer/Backend/Program.cs
+++ invoicer/Backend/Program.cs
@@ -32,14 +32,10 @@ namespace Backend
 
 			var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (app.Environment.IsDevelopment())
-			{
-				app.UseSwagger();
-				app.UseSwaggerUI();
-			}
+			app.UseSwagger();
+			app.UseSwaggerUI();
 
-			app.UseHttpsRedirection();
+			//app.UseHttpsRedirection();
 
 			app.UseAuthorization();
 
