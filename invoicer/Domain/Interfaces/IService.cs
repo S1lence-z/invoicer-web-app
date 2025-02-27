fba5c96d8c1edd3d091980b61353747c2cde454c
S1lence <j3.zelenka@gmail.com>
2025-02-28 00:39:21 +0100

    chore: update ico to be string

--- invoicer/Domain/Interfaces/IService.cs
+++ invoicer/Domain/Interfaces/IService.cs
@@ -4,7 +4,7 @@
 	{
 		Task<TType?> GetByIdAsync(TKey id);
 		Task<IList<TType>> GetAllAsync();
-		Task<TType> CreateAsync(TType obj);
+		Task<TType?> CreateAsync(TType obj);
 		Task<TType> UpdateAsync(TKey id, TType obj);
 		Task<bool> DeleteAsync(TKey id);
 	}
--- invoicer/Domain/Models/Entity.cs
+++ invoicer/Domain/Models/Entity.cs
@@ -3,7 +3,7 @@
     public class Entity
     {
 		public int Id { get; set; }
-		public int Ico { get; set; }
+		public string Ico { get; set; } = string.Empty;
 		public string Name { get; set; } = string.Empty;
 		public string Email { get; set; } = string.Empty;
 		public string PhoneNumber { get; set; } = string.Empty;
