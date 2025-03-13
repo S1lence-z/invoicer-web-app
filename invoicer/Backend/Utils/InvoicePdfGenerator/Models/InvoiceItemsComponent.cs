287f19aebf21c59f8f904e4573bf3ad068d0e24f
S1lence-z <j3.zelenka@gmail.com>
2025-03-13 23:10:07 +0100

    feat: add vat and vat prices to the generated invoice

--- invoicer/Backend/Utils/InvoicePdfGenerator/Models/InvoiceItemsComponent.cs
+++ invoicer/Backend/Utils/InvoicePdfGenerator/Models/InvoiceItemsComponent.cs
@@ -1,13 +1,16 @@
 ﻿using Domain.Enums;
 using Domain.Models;
 using QuestPDF.Fluent;
+using QuestPDF.Helpers;
 using QuestPDF.Infrastructure;
 
 namespace Backend.Utils.InvoicePdfGenerator.Models
 {
 	public class InvoiceItemsComponent(ICollection<InvoiceItem> items, Currency invoiceCurrency) : IComponent
 	{
-		private void ComposeTable(IContainer container)
+		private decimal TotalPriceWithoutVat { get; set; }
+
+		private void ComposeInvoiceItemsTable(IContainer container)
 		{
 			container.Table(table =>
 			{
@@ -18,6 +21,7 @@ namespace Backend.Utils.InvoicePdfGenerator.Models
 					columns.ConstantColumn(50); // Unit
 					columns.ConstantColumn(100); // Unit Price
 					columns.ConstantColumn(100); // Total Price
+					columns.ConstantColumn(50); // Vat Rate
 				});
 				table.Header(header =>
 				{
@@ -26,6 +30,7 @@ namespace Backend.Utils.InvoicePdfGenerator.Models
 					header.Cell().Text("Unit").Bold();
 					header.Cell().Text("Unit Price").Bold();
 					header.Cell().Text("Total").Bold();
+					header.Cell().Text("VAT Rate").Bold();
 				});
 				foreach (var item in items)
 				{
@@ -34,17 +39,56 @@ namespace Backend.Utils.InvoicePdfGenerator.Models
 					table.Cell().Text(item.Unit);
 					table.Cell().Text($"{item.UnitPrice} {invoiceCurrency}");
 					table.Cell().Text($"{(item.Quantity * item.UnitPrice)} {invoiceCurrency}");
+					table.Cell().Text($"{(int)(item.VatRate * 100)}%");
 				}
 			});
 		}
+
+		private void ComposeInvoicePriceTable(IContainer container)
+		{
+			var totalPrice = items.Sum(item => item.Quantity * item.UnitPrice);
+			var vatPrice = items.Sum(item => item.Quantity * item.UnitPrice * item.VatRate);
+			var totalPriceWithVat = totalPrice + vatPrice;
+
+			container.Table(table =>
+			{
+				table.ColumnsDefinition(columns =>
+				{
+					columns.RelativeColumn();
+					columns.RelativeColumn();
+					columns.RelativeColumn();
+				});
+
+				table.Header(header =>
+				{
+					header.Cell().Text("Total Price without VAT").Bold().AlignCenter();
+					header.Cell().Text("Total VAT").Bold().AlignCenter();
+					header.Cell().Text("Total Price with VAT").Bold().AlignCenter();
+				});
+				// Row with prices
+				table.Cell().Text($"{totalPrice} {invoiceCurrency}").AlignCenter();
+				table.Cell().Text($"{vatPrice:F1} {invoiceCurrency}").AlignCenter();
+				table.Cell().Text($"{totalPriceWithVat:F1} {invoiceCurrency}").AlignCenter();
+				// The final row
+				table.Cell().ColumnSpan(3)
+					.PaddingTop(20)
+					.AlignRight()
+					.Padding(10)
+					.Text(text =>
+					{
+						text.Span("Final Price: ").Bold().FontSize(14);
+						text.Span($"{totalPriceWithVat:F1} {invoiceCurrency}").FontSize(14);
+					});
+			});
+		}
+
 
 		public void Compose(IContainer container)
 		{
 			container.Column(col =>
 			{
-				col.Item().PaddingBottom(20).Element(ComposeTable);
-				var totalPrice = items.Sum(item => item.Quantity * item.UnitPrice);
-				col.Item().Text($"Total Price: {totalPrice} {invoiceCurrency}").AlignRight().Bold().FontSize(18);
+				col.Item().PaddingBottom(100).Element(ComposeInvoiceItemsTable);
+				col.Item().Element(ComposeInvoicePriceTable);
 			});
 		}
 	}
