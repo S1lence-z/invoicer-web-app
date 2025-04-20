63794b97365ae16eb077ef1d7c87717eb17e15a9
S1lence-z <j3.zelenka@gmail.com>
2025-04-21 01:51:57 +0200

    fix: fix numbering scheme form errors, rename scheme form model

---
+++ invoicer/Frontend/Models/FormModels/NumberingSchemeFormModel.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Domain.Enums;
+using Application.DTOs;
+
+namespace Frontend.Models.FormModels
+{
+	public class NumberingSchemeFormModel
+	{
+		public int Id { get; set; }
+
+		[RegularExpression(@"^.*$", ErrorMessage = "Invalid input format")]
+		public string Prefix { get; set; } = string.Empty;
+
+		public bool UseSeperator { get; set; } = true;
+
+		[RegularExpression(@"^.*$", ErrorMessage = "Invalid input format")]
+		public string Seperator { get; set; } = "-";
+
+		[EnumDataType(typeof(Position), ErrorMessage = "Invalid sequence position")]
+		public Position SequencePosition { get; set; } = Position.Start;
+
+		[Range(1, 10, ErrorMessage = "Sequence padding must be between 1 and 10")]
+		public int SequencePadding { get; set; } = 3;
+
+		[EnumDataType(typeof(YearFormat), ErrorMessage = "Invalid year format")]
+		public YearFormat YearFormat { get; set; } = YearFormat.FourDigit;
+
+		public bool IncludeMonth { get; set; } = true;
+
+		[EnumDataType(typeof(ResetFrequency), ErrorMessage = "Invalid reset frequency")]
+		public ResetFrequency ResetFrequency { get; set; } = ResetFrequency.Yearly;
+
+		public bool IsDefault { get; set; } = false;
+
+		public static NumberingSchemeFormModel FromDto(NumberingSchemeDto dto)
+		{
+			return new NumberingSchemeFormModel
+			{
+				Id = dto.Id,
+				Prefix = dto.Prefix,
+				UseSeperator = dto.UseSeperator,
+				Seperator = dto.Seperator,
+				SequencePosition = dto.SequencePosition,
+				SequencePadding = dto.SequencePadding,
+				YearFormat = dto.YearFormat,
+				IncludeMonth = dto.IncludeMonth,
+				ResetFrequency = dto.ResetFrequency,
+				IsDefault = dto.IsDefault
+			};
+		}
+
+		public NumberingSchemeDto ToDto()
+		{
+			return new NumberingSchemeDto
+			{
+				Id = Id,
+				Prefix = Prefix,
+				UseSeperator = UseSeperator,
+				Seperator = Seperator,
+				SequencePosition = SequencePosition,
+				SequencePadding = SequencePadding,
+				YearFormat = YearFormat,
+				IncludeMonth = IncludeMonth,
+				ResetFrequency = ResetFrequency,
+				IsDefault = IsDefault
+			};
+		}
+	}
+}
