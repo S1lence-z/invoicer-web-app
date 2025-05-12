36567a833b72eaedf8222c92a6bbd1d6b89dc8cb
S1lence-z <j3.zelenka@gmail.com>
2025-05-12 18:21:35 +0200

    feat: visualize default scheme better and add update function for seperator

--- invoicer/Shared/DTOs/NumberingSchemeDto.cs
+++ invoicer/Shared/DTOs/NumberingSchemeDto.cs
@@ -4,10 +4,11 @@ namespace Shared.DTOs
 {
 	public record class NumberingSchemeDto
 	{
+		public static readonly string DefaultSeparator = "-";
 		public int Id { get; set; }
 		public string Prefix { get; set; } = string.Empty;
 		public bool UseSeperator { get; set; } = true;
-		public string Seperator { get; set; } = "-";
+		public string Seperator { get; set; } = DefaultSeparator;
 		public Position SequencePosition { get; set; } = Position.Start;
 		public int SequencePadding { get; set; } = 3;
 		public YearFormat YearFormat { get; set; } = YearFormat.FourDigit;
