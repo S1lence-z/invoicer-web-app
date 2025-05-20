5f4e87e586af38a19d1519233b20d18c1b7e1ddd
S1lence <j3.zelenka@gmail.com>
2025-05-20 13:41:27 +0200

    [wip] feat: add a base class for the form models

---
+++ invoicer/Frontend/Models/Base/FormModelBase.cs
@@ -0,0 +1,30 @@
+﻿namespace Frontend.Models.Base
+{
+	public abstract class FormModelBase<TFormModel, TDto>
+		where TFormModel : FormModelBase<TFormModel, TDto>, new()
+		where TDto : class
+	{
+		public virtual int Id { get; set; }
+
+		public bool IsNewEntity => Id == 0;
+
+		public static TFormModel FromDto(TDto dto)
+		{
+			TFormModel formModel = new();
+			if (dto is not null)
+				formModel.PopulateFromDto(dto);
+			return formModel;
+		}
+
+		public void PopulateFromDto(TDto dto) => MapPropertiesFromDto(dto);
+
+		protected abstract void MapPropertiesFromDto(TDto dto);
+		protected abstract void ResetProperties();
+
+		public virtual void ClearModel()
+		{
+			Id = 0;
+			ResetProperties();
+		}
+	}
+}
